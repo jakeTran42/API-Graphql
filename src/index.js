9fa539466620f7f24f053421d9984ebf6b8db20f
Jake Tran <jake.tranehs@gmail.com>
2019-01-30 03:15:15 -0800

    add server

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { GraphQLServer } = require('graphql-yoga')
+var express = require('express')
+var app = express()
+
+// 1
+const typeDefs = `
+type Query {
+  info: String!
+}
+`
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`
+  }
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
